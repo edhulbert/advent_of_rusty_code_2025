e2a9c8db03d1e37fc8df6cebd13dd52c62731016
edhulbert <edward.hulbert@hotmail.co.uk>
2025-12-02 14:23:34 +0100

    tidied challenge 2

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -12,6 +12,7 @@ fn challenge_2() -> std::io::Result<()> {
 
     let file = File::open("input/day01_test.txt")?;
     let buf_reader: BufReader<File> = BufReader::new(file);
+    
     let mut pos: i16 = 50;
     let mut zeros: i16 = 0;
 
@@ -23,40 +24,24 @@ fn challenge_2() -> std::io::Result<()> {
         pos = new_pos;
         zeros += num_zeros;
     }
+    let elapsed: std::time::Duration = now.elapsed();
 
     println!("password: {}", zeros);
 
-    let elapsed = now.elapsed();
     println!("elapsed: {:.2?}", elapsed);
     Ok(())
 }
 
 fn determine_new_pos_and_zeros(pos: i16, a: i16, dir: &str) -> (i16, i16) {
-    let (new_pos, crossings) = if dir == "L" {
-        let total_pos = pos - a; // -1
-        let crossings = if pos == 0 {
-            (100 - total_pos) / 100 - 1
-        } else {
-            (100 - total_pos) / 100
-        };
-        (determine_negative_pos(total_pos), crossings)
+    let delta: i16 = if dir == "L" { -a } else { a };
+    let total_pos = pos + delta;
+    let crossings = if delta > 0 {
+        total_pos / 100
+    } else if pos == 0 {
+        (100 - total_pos) / 100 - 1
     } else {
-        let total_pos: i16 = pos + a;
-        (determine_positive_pos(total_pos), total_pos / 100)
+        (100 - total_pos) / 100
     };
-    println!("new: {}, crossings: {}", new_pos, crossings);
+    let new_pos = total_pos.rem_euclid(100);
     (new_pos, crossings)
-}
-
-fn determine_negative_pos(pos: i16) -> i16 {
-    return if pos < 0 { 
-        let num: i16 = 100 + (pos % 100);
-        if num == 100 { 0 } else { num }
-    } else {
-        pos
-    };
-}
-
-fn determine_positive_pos(pos: i16) -> i16 {
-    pos.rem_euclid(100)
 }
